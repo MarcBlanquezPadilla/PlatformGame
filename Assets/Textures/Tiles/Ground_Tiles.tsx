17ff8c859d1363fd00d3edec7e0f623ef9c504b1
Marc Blánquez <102283326+MarcBlanquezPadilla@users.noreply.github.com>
2024-10-14 10:23:43 +0200

    Map

---
+++ Assets/Textures/Tiles/Ground_Tiles.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Ground_Tiles" tilewidth="16" tileheight="16" tilecount="110" columns="11">
+ <image source="Ground_Tiles.png" width="176" height="160"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="orange brick" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="orange plat" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="55" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="56" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="57" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="66" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="67" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="68" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="77" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="78" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="79" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="88" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="89" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="90" wangid="0,0,0,0,0,2,0,0"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="orange column" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="58" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="69" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="80" wangid="1,0,0,0,0,0,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
